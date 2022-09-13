d162d1bd6e237d1969f3280789c2a913a3ebb132
Refaldy Bagas R <refaldy.bagas10@gmail.com>
2022-09-14 02:51:21 +0700

    fetch-ticker feature done

--- routes/index.routes.js
+++ routes/index.routes.js
@@ -1,10 +1,12 @@
 const router = require('express').Router();
 const newsRoutes = require('./news.routes');
 const authRoutes = require('./auth.routes');
+const tickerRoutes = require('./ticker.routes');
 const errorHandler = require('../middleware/erorr.handler');
 
 router.use(authRoutes);
 router.use('/news', newsRoutes);
+router.use('/tickers', tickerRoutes);
 router.use(errorHandler);
 
 module.exports = router;

2ec575937f9eacacdbe2e3c490275c3fd97cd886
Ikhraaazh <ikhsanrafi06@gmail.com>
2022-09-14 07:30:27 +0800

    login feature done

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,4 +1,6 @@
 const { User } = require("../models");
+const {compareHash} = require('../helpers/bcrypt')
+const {createToken} = require('../helpers/jwt')
 
 class Controller {
   static async register(req, res, next) {
@@ -12,6 +14,27 @@ class Controller {
       res.status(201).json({id: newUser.id, email: newUser.email})
     } catch (error) {
       next(error);
+    }
+  }
+
+  static async login(req, res, next) {
+    try {
+      const {email, password} = req.body
+      if(!email) throw {name: "Email is required"}
+      if(!password) throw {name: "Password is required"}
+
+      const user = await User.findOne({where: {email}})
+      if(!user) throw {name: "Invalid email/password"}
+
+      const compared = compareHash(password, user.password)
+      if(!compared) throw {name: "Invalid email/password"}
+
+      const payload = {id: user.id}
+      const access_token = createToken(payload)
+
+      res.status(200).json({access_token})
+    } catch (error) {
+      next(error)
     }
   }
 }

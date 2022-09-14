4d6a5fe638552495eb5376d410a00aca621a6168
nur mizwari <nurmizwari04@gmail.com>
2022-09-14 14:15:01 +0700

    coba deploy heroku

--- models/inventory.js
+++ models/inventory.js
@@ -9,6 +9,7 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate(models) {
       // define association here
+      Inventory.belongsTo(models.Category);
     }
   }
   Inventory.init(

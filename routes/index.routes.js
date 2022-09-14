8fdee06a1ed5b275e956f6425f516e3f808b13e3
Refaldy Bagas R <refaldy.bagas10@gmail.com>
2022-09-14 12:47:47 +0700

    calculate lumpsum done

--- routes/index.routes.js
+++ routes/index.routes.js
@@ -2,11 +2,13 @@ const router = require('express').Router();
 const newsRoutes = require('./news.routes');
 const authRoutes = require('./auth.routes');
 const tickerRoutes = require('./ticker.routes');
+const calculatorRoutes = require('./calculator.routes');
 const errorHandler = require('../middleware/erorr.handler');
 
 router.use(authRoutes);
 router.use('/news', newsRoutes);
 router.use('/tickers', tickerRoutes);
+router.use('/calculators', calculatorRoutes);
 router.use(errorHandler);
 
 module.exports = router;

cd6a2366dcacd5411c874fad2a3aa3abd0dab4f8
Ikhraaazh <ikhsanrafi06@gmail.com>
2022-09-15 05:34:42 +0800

    updated paid status feature done

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,7 +1,12 @@
 const router = require("express").Router()
 const Controller = require('../controllers/userController')
+const authentication = require('../middlewares/authentication')
 
 router.post('/register', Controller.register)
 router.post('/login', Controller.login)
+
+router.use(authentication)
+
+router.patch('/status', Controller.paid)
 
 module.exports = router

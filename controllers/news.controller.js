a408158e8d6e7e904f6431626d4a78cd0e57d86f
Refaldy Bagas R <refaldy.bagas10@gmail.com>
2022-09-14 15:02:13 +0700

    fix news feature

--- controllers/news.controller.js
+++ controllers/news.controller.js
@@ -2,15 +2,25 @@ const axios = require('axios');
 
 class NewsController {
   static async index(req, res) {
-    try{
-      const {data} = await axios.get('https://api.marketaux.com/v1/news/all', {
+    try {
+      // const {data} = await axios.get('https://api.marketaux.com/v1/news/all', {
+      //   params: {
+      //     api_token: process.env.NEWS_API,
+      //     language: 'en'
+      //   }
+      // });
+      const {data: {data: {mostPopularEntries: {assets}}}} = await axios.get('https://cnbc.p.rapidapi.com/news/v2/list-trending', {
         params: {
-          api_token: process.env.NEWS_API,
-          language: 'en'
+          tag: 'Articles',
+          count: 15
+        },
+        headers: {
+          'X-RapidAPI-Key': process.env.CNBC_API_KEY,
+          'X-RapidAPI-Host': 'cnbc.p.rapidapi.com'
         }
       });
-      res.status(200).send(data);
-    }catch (e) {
+      res.status(200).send(assets);
+    } catch (e) {
       res.status(500).json({
         message: 'Internal Server Error'
       });

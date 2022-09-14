708ea0c10b220ea43860b872035f88bee0344682
nur mizwari <nurmizwari04@gmail.com>
2022-09-15 00:23:26 +0700

    pagination

--- controller/inventory.js
+++ controller/inventory.js
@@ -1,13 +1,54 @@
 const { Inventory, Category } = require("../models");
+const { Op } = require("sequelize");
 class inventoryController {
   static async getInventory(req, res) {
-    try {
-      let data = await Inventory.findAll();
-      res.status(200).json(data);
-    } catch (error) {
-      console.log(error);
-      res.status(500).json({ message: "Internal server error" });
+    // try {
+    //   let data = await Inventory.findAll();
+    //   res.status(200).json(data);
+    // } catch (error) {
+    //   console.log(error);
+    //   res.status(500).json({ message: "Internal server error" });
+    // }
+
+    const getPagination = (page, size) => {
+      const limit = size ? +size : 8;
+      // const offset = page || +page !== 0 ? (page - 1) * limit : 0;
+      const offset = page ? page * limit : 0;
+      return { limit, offset };
+    };
+
+    const getPagingData = (data, page, limit) => {
+      const { count: totalItems, rows: Inventory } = data;
+      const currentPage = page ? +page : 1;
+      const totalPages = Math.ceil(totalItems / limit);
+      return { totalItems, Inventory, totalPages, currentPage };
+    };
+
+    const { page, size, title } = req.query;
+    let condition = {
+      name: { [Op.iLike]: `%${title}%` },
+    };
+    if (!req.query.title) {
+      condition = {
+        stock: { [Op.gt]: 0 },
+      };
     }
+
+    const { limit, offset } = getPagination(page, size);
+    let data = await Inventory.findAndCountAll({
+      where: condition,
+      limit,
+      offset,
+    });
+    const response = getPagingData(data, page, limit);
+
+    // let data = await Transportation.findAll(option);
+    // console.log(data);
+    if (!data) {
+      throw { name: "data not found" };
+    }
+
+    res.status(200).json(response);
   }
   static async AddInventory(req, res) {
     try {

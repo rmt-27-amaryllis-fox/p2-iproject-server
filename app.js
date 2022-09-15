11389527ea3ffa65aa631b1786fad66fdddeab70
Arifinwidy02 <arifinwidy@gmail.com>
2022-09-15 11:31:08 +0700

    redeploy

--- app.js
+++ app.js
@@ -9,9 +9,9 @@ const route = require("./routes/route");
 const port = process.env.PORT || 3000;
 const errorHandler = require("./middleware/errorHandler");
 
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cors());
 app.use(route);
 app.use(errorHandler);
 

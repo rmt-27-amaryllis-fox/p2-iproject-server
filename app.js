e60f58c1b67d9c249486fdc27b9b0e426c079904
Arifinwidy02 <arifinwidy@gmail.com>
2022-09-15 12:06:08 +0700

    add-dotenv

--- app.js
+++ app.js
@@ -2,9 +2,9 @@ if (process.env.NODE_ENV !== "production") {
   require("dotenv").config();
 }
 
+const cors = require("cors");
 const express = require("express");
 const app = express();
-const cors = require("cors");
 const route = require("./routes/route");
 const port = process.env.PORT || 3000;
 const errorHandler = require("./middleware/errorHandler");
